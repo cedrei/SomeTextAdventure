bd6af6e9ae71fe11384dbf92f8539fc52e7cb86a
Cedrei <einar.ceder@gmail.com>
2019-03-26 14:19:36 +0100

    Add writeParagraph function

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
+function writeParagraph(text) {
+	document.write("<p>"+text+"</p><br>")
+}
+
 document.write('Many many years ago, In a country somewhat far away...<br>') 
 document.write('In the far south')
